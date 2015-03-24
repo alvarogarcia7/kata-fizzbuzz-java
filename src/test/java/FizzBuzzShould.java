9ad927f91b3421de8df5e44332d8328c5b0daf6f
alvaro garcia <derecerca@gmail.com>
2015-03-24 18:27:59 +0100

    initial commit: gitignore, project with dependencies, red: return_the_same_number_for_an_input_not_matching_divisors

---
+++ src/test/java/FizzBuzzShould.java
@@ -0,0 +1,17 @@
+import org.junit.Test;
+
+import static org.hamcrest.MatcherAssert.assertThat;
+import static org.hamcrest.Matchers.is;
+
+/**
+ * Created by alvaro on 24/03/15.
+ */
+public class FizzBuzzShould {
+
+	@Test
+	public void return_the_same_number_for_an_input_not_matching_divisors () {
+
+		assertThat(fizzBuzz(1), is(1));
+	}
+}
+
