4e17886ba644a1ff84efb7e9e2138c1d9eff19d4
alvaro garcia <derecerca@gmail.com>
2015-03-24 18:30:07 +0100

    green

--- src/test/java/FizzBuzzShould.java
+++ src/test/java/FizzBuzzShould.java
@@ -11,7 +11,11 @@ public class FizzBuzzShould {
 	@Test
 	public void return_the_same_number_for_an_input_not_matching_divisors () {
 
-		assertThat(fizzBuzz(1), is(1));
+		assertThat(fizzBuzz(1), is("1"));
+	}
+
+	private String fizzBuzz (final int num) {
+		return "1";
 	}
 }
 
